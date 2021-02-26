c098d32c0be679bb53b19a90f4d2d1966f77d0a8
UsmanGTA <hi@usmanjabbar.com>
2021-02-27 00:40:41 +0500

    Added updates for task 0

--- assets/index.js
+++ assets/index.js
@@ -8,41 +8,35 @@
 let api = {};
 let endpoints = ['quotes', 'popular-tutorials', 'latest-videos', 'courses'];
 apiSetter = () => {
-	let base_url = 'https://smileschool-api.hbtn.info/';
-
 	apiGetter = (data, section) => { api[section] = data; }
 
 	for (const endpoint of endpoints) {
 		$.ajax({
-			url: base_url + endpoint,
-			success: (data) => { apiGetter(data, endpoint); }
+			url: `https://smileschool-api.hbtn.info/${endpoint}`,
+			success: (data) => { apiGetter(data, endpoint); },
+			fail: () => { alert(`The backend is not reachable`); }
 		})
 	}
 }
-
 /**
  * This section adds content to the testimonals/quotes section
  */
-getQuotes = () => {
+function getQuotes() {
 	for (const element of api.quotes){
 		$('#testimonials .carousel-inner').append(
-			$('</div>', {class: 'carousel-item'}).append(
-				$('</div>', {class: 'd-flex flex-wrap flex-md-nowrap align-items-center justify-content-center'}).append(
-					$('</div>', {class: 'testimonial_profile_pic m-4'}).append(
-						$('</img>', {class: 'rounded-circle', source: element.pic_url})
+			$('<div>', {class: 'carousel-item'}).append(
+				$('<div>', {class: 'd-flex flex-wrap flex-md-nowrap align-items-center justify-content-center'}).append(
+					$('<div>', {class: 'testimonial_profile_pic m-4'}).append(
+						$('<img>', {class: 'rounded-circle', src: element.pic_url})
 					),
-					$('</div>', {class: 'testimonal_text'}).append(
-						$('</blockquote>', {class: 'testimonal_quotes', text: element.text}),
-						$('</h4>', {class: 'profile_name m-0', text: element.name}),
-						$('</p>', {class: 'profession font-italic', text: element.title})
-					)
-				)
-			)
-		)
+					$('<div>', {class: 'testimonal_text'}).append(
+						$('<blockquote>', {class: 'testimonal_quotes', text: element.text}),
+						$('<h4>', {class: 'profile_name m-0', text: element.name}),
+						$('<p>', {class: 'profession font-italic', text: element.title})
+		))))
 	}
+	$('.carousel-item:nth-child(1)').addClass('active');
 }
 
-$(document).load( () => {
-	apiSetter();
-	getQuotes();
-})
+apiSetter();
+getQuotes();
